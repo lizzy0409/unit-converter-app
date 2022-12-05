ae6630e5eb23ed4a25ebe0bdd0fe5a4fb6005d5b
ennoukra <ghaniennoukra@gmail.com>
2022-12-05 21:01:53 +0100

    disrance activity

--- app/src/main/java/com/example/miniprojectdom/distanceActivity.java
+++ app/src/main/java/com/example/miniprojectdom/distanceActivity.java
@@ -3,9 +3,12 @@ package com.example.miniprojectdom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class distanceActivity extends AppCompatActivity {
-
+    private Spinner spinner;
+    private static final String[] uniteDistance = {"mm", "dm", "cm", "m", "dam", "Km"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
